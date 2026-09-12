2a783d486b5c9410aa51bdc9f3f52b85239cb1da
P2wn <racecarboy77@gmail.com>
2026-09-12 17:50:00 -0400

    Remove blocking profile lookup from reply command

--- src/main/java/dev/rosewood/rosechat/command/command/ReplyCommand.java
+++ src/main/java/dev/rosewood/rosechat/command/command/ReplyCommand.java
@@ -1,6 +1,5 @@
 package dev.rosewood.rosechat.command.command;
 
-import dev.rosewood.rosechat.RoseChat;
 import dev.rosewood.rosechat.command.RoseChatCommand;
 import dev.rosewood.rosechat.message.MessageUtils;
 import dev.rosewood.rosechat.message.RosePlayer;
@@ -10,8 +9,6 @@ import dev.rosewood.rosegarden.command.framework.ArgumentsDefinition;
 import dev.rosewood.rosegarden.command.framework.CommandContext;
 import dev.rosewood.rosegarden.command.framework.CommandInfo;
 import dev.rosewood.rosegarden.command.framework.annotation.RoseExecutable;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 
 public class ReplyCommand extends RoseChatCommand {
 
@@ -39,30 +36,14 @@ public class ReplyCommand extends RoseChatCommand {
 
     @RoseExecutable
     public void execute(CommandContext context, String message) {
-        Bukkit.getScheduler().runTaskAsynchronously(RoseChat.getInstance(), () -> {
-            RosePlayer player = new RosePlayer(context.getSender());
-            String targetName = player.getPlayerData().getReplyTo();
-            if (targetName == null) {
-                player.sendLocaleMessage("command-reply-no-one");
-                return;
-            }
+        RosePlayer player = new RosePlayer(context.getSender());
+        String targetName = player.getPlayerData().getReplyTo();
+        if (targetName == null) {
+            player.sendLocaleMessage("command-reply-no-one");
+            return;
+        }
 
-            OfflinePlayer target = Bukkit.getOfflinePlayer(targetName);
-
-            if (context.getSender().hasPermission("rosechat.togglemessage.bypass")) {
-                MessageUtils.sendPrivateMessage(player, targetName, message);
-                return;
-            }
-
-            this.getAPI().getPlayerData(target.getUniqueId(), data -> {
-                if (data != null && !data.canBeMessaged()) {
-                    player.sendLocaleMessage("command-togglemessage-cannot-message");
-                    return;
-                }
-
-                MessageUtils.sendPrivateMessage(player, targetName, message);
-            });
-        });
+        MessageUtils.sendPrivateMessage(player, targetName, message);
     }
 
 }
